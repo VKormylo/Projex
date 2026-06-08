759955190150589a1fe76d8fc99b106eb30e0a45
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2026-06-08 18:59:11 +0300

    feat: setup react client application with vite

---
+++ client/src/vite-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_PATH?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
