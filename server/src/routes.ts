9a95ba78b9ffa48cb646c960bd9427a34d581584
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2026-06-08 18:56:26 +0300

    feat: add api routing, swagger documentation and health tests

---
+++ server/src/routes.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+
+import { authRouter } from "./modules/auth/auth.routes";
+import { usersRouter, rolesRouter } from "./modules/users/users.routes";
+import { teamsRouter } from "./modules/teams/teams.routes";
+import { projectsRouter } from "./modules/projects/projects.routes";
+import { sprintsRouter } from "./modules/sprints/sprints.routes";
+import { tasksRouter } from "./modules/tasks/tasks.routes";
+import { commentsRouter } from "./modules/comments/comments.routes";
+import { adminRouter } from "./modules/admin/admin.routes";
+import { authenticate } from "./middleware/auth";
+
+export const apiRouter = Router();
+
+apiRouter.use("/auth", authRouter);
+apiRouter.use("/roles", authenticate, rolesRouter);
+apiRouter.use("/users", authenticate, usersRouter);
+apiRouter.use("/teams", authenticate, teamsRouter);
+apiRouter.use("/projects", authenticate, projectsRouter);
+apiRouter.use("/sprints", authenticate, sprintsRouter);
+apiRouter.use("/tasks", authenticate, tasksRouter);
+apiRouter.use("/comments", authenticate, commentsRouter);
+apiRouter.use("/admin", authenticate, adminRouter);
